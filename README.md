9b8f938dded95a28e47d14da83a973f02e59a16b
Keith Brings <keith.brings@noizu.com>
2017-07-26 14:52:47 -0700

    Setting up submodule folder for api-reference, incrementing version to 0.1.0, updating ex_doc

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # RuleEngine
 Noizu Scripting and Rule Engine for Business and Interactive Scripting
+
+## Additional Documentation
+* [Api Documentation](http://noizu.github.io/RuleEngine)
--- mix.exs
+++ mix.exs
@@ -3,29 +3,38 @@ defmodule Noizu.RuleEngine.Mixfile do
 
   def project do
     [app: :noizu_rule_engine,
-     version: "0.0.1",
+     version: "0.1.0",
      elixir: "~> 1.3",
      package: package(),
      deps: deps(),
-     description: "Noizu Rule Engine"
+     description: "Noizu Rule Engine",
+     docs: docs()
    ]
-  end
+  end # end project
 
   defp package do
     [
       maintainers: ["noizu"],
-      licenses: ["Apache License 2.0"],
+      licenses: ["MIT"],
       links: %{"GitHub" => "https://github.com/noizu/RuleEngine"}
     ]
-  end
+  end # end package
 
   def application do
     [ applications: [:logger] ]
-  end
+  end # end application
 
   defp deps do
-    [ { :ex_doc, "~> 0.11", only: [:dev] } ]
-  end
+    [
+      {:ex_doc, "~> 0.16.2", only: [:dev], optional: true}, # Documentation Provider
+      {:markdown, github: "devinus/markdown", only: [:dev], optional: true}, # Markdown processor for ex_doc
+    ]
+  end # end deps
 
-
-end
+  defp docs do
+    [
+      source_url_pattern: "https://github.com/noizu/RuleEngine/blob/master/%{path}#L%{line}",
+      extras: ["README.md"]
+    ]
+  end # end docs
+end # end defmodule
